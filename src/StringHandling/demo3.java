2c46b9cce151c1c3ebb2afaedb29dd731630815a
Vaishnavi <VaishnaviKamble2403@gmail.com>
2024-12-28 21:44:36 +0530

    add demo3.java

---
+++ src/StringHandling/demo3.java
@@ -0,0 +1,10 @@
+package StringHandling;
+
+public class demo3
+{
+    public static void main(String[] args) {
+        String str =" Good Hava a Nice day ";
+        System.out.println(str);//toString method
+
+    }
+}
