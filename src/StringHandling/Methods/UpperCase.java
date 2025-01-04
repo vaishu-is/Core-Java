40637368342fa0eb6c6ad5a7525eb46a20e59a5d
Vaishnavi <VaishnaviKamble2403@gmail.com>
2025-01-04 15:41:35 +0530

    add UpperCase.java

--- src/StringHandling/Methods/UpperCase.java
+++ src/StringHandling/Methods/UpperCase.java
@@ -1,4 +1,11 @@
 package StringHandling.Methods;
 
 public class UpperCase {
+    public static void main(String[] args) {
+
+        String str="Wellcome to India";
+        str= str.toUpperCase();
+        System.out.println(str);
+        // It make the String into upper CASE aaa--->> AAA
+    }
 }
