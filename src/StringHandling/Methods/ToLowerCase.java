784f96429eb7008a89606cb5b6231dba23a3b2b5
Vaishnavi <VaishnaviKamble2403@gmail.com>
2025-01-04 15:44:18 +0530

    add ToLowerCase.java

---
+++ src/StringHandling/Methods/ToLowerCase.java
@@ -0,0 +1,10 @@
+package StringHandling.Methods;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String str="I LOVE INDIA";
+        str=str.toLowerCase();
+        System.out.println(str);
+    }
+}
+
