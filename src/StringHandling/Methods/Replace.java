eb682840f21aae45ec7d436b9edb8ab6f39b85b5
Vaishnavi <VaishnaviKamble2403@gmail.com>
2025-01-04 16:10:31 +0530

    add SubString.java

---
+++ src/StringHandling/Methods/Replace.java
@@ -0,0 +1,14 @@
+package StringHandling.Methods;
+
+public class Replace {
+    public static void main(String[] args) {
+        //It will replace the old char into new Char String
+
+        String str1="Tiger";
+       str1= str1.replace('i','@');
+        System.out.println(str1);
+
+
+
+    }
+}
---
+++ src/StringHandling/Methods/SubString.java
@@ -0,0 +1,10 @@
+package StringHandling.Methods;
+
+public class SubString {
+    public static void main(String[] args) {
+
+        String str ="Python Master Class";
+        String SubString=str.substring(0,3);
+        System.out.println(SubString);
+    }
+}
